8281641d2d09c35de2352a50a421bacf7eb00dbe
Dou1926 <129563497+Dou1926@users.noreply.github.com>
2023-06-21 19:43:58 -0500

    union con el buscador
    union con el buscador, profe lo uni en otro js por que  me dañaba el boton del aside

---
+++ buscador.js
@@ -0,0 +1,7 @@
+function search() {   
+    var searchTerm = document.getElementById('campo-de-busqueda').value;
+    var searchResults = document.getElementById('searchResults');
+    searchResults.innerText = 'El resultado es: ' + searchTerm;
+  }
+  
+
